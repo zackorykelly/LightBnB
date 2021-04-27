4677952e9d730113baacfb9721d62bf58714aac7
Zackory Kelly <zackkelly@live.ca>
2021-04-27 19:03:13 +0000

    Add queries for LighthouseBnB SELECT

---
+++ 1_queries/5_all_my_reservations.sql
@@ -0,0 +1,9 @@
+SELECT reservations.*, properties.*, avg(property_reviews.rating) AS average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON property_reviews.property_id = properties.id
+WHERE reservations.guest_id = 1
+AND reservations.end_date < now()::date
+GROUP BY reservations.id, properties.id
+ORDER BY reservations.start_date ASC
+LIMIT 10
--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,28 +1,28 @@
 INSERT INTO users(name, email, password)
 VALUES(
-'J.P. Wiser', jpwiser@gmail.com, $2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.
+'J.P. Wiser', 'jpwiser@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'
 ),(
-'Jack Daniel', jackdaniel@gmail.com, $2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.
+'Jack Daniel', 'jackdaniel@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'
 ),(
-'Jim Beam' jimbeam@gmail.com, $2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.
+'Jim Beam', 'jimbeam@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'
 );
 
 INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
 VALUES(
-2, 'Castle Black', 'description', 'https://www.google.com', 'https://www.google.com', 500, 3, 2, 3, 'Canada', 'Yonge St.', 'Toronto', 'Ontario', 'M4P 2J1'
+2, 'Castle Black', 'description', 'https://www.google.com', 'https://www.google.com', 500, 3, 2, 3, 'Canada', 'Yonge St.', 'Toronto', 'Ontario', 'M4P 2J1', TRUE
 ),(
-2, 'Red Keep', 'description', 'https://www.google.com', 'https://www.google.com', 1, 0, 0, 37, 'Canada', 'Yonge St.', 'Nanaimo', 'British Colombia', 'M2P 2J1'
+2, 'Red Keep', 'description', 'https://www.google.com', 'https://www.google.com', 1, 0, 0, 37, 'Canada', 'Yonge St.', 'Nanaimo', 'British Colombia', 'M2P 2J1', TRUE
 ),(
-1, 'Fraggle Rock', 'description', 'https://www.google.com', 'https://www.google.com', 10000, 42, 200, 0, 'Mars', 'Yonge St.', 'Toronto', 'Ontario', 'M4O 2J1'
+1, 'Fraggle Rock', 'description', 'https://www.google.com', 'https://www.google.com', 10000, 42, 200, 0, 'Mars', 'Yonge St.', 'Toronto', 'Ontario', 'M4O 2J1', TRUE
 );
 
 INSERT INTO reservations(start_date, end_date, property_id, guest_id)
 VALUES(
-2018-09-11, 2018-09-26, 2, 3
+'2018-09-11', '2018-09-26', 2, 3
 ),(
-2018-09-11, 2018-09-26, 3, 3
+'2018-09-11', '2018-09-26', 3, 3
 ),(
-2018-09-11, 2018-09-26, 3, 2
+'2018-09-11', '2018-09-26', 3, 2
 );
 
 INSERT INTO property_reviews(guest_id, property_id, reservation_id, rating, message)
