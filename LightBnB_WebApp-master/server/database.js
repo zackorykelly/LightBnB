f20aac7c5a2e54935f77a4f2e599404977a55633
Zackory Kelly <zackkelly@live.ca>
2021-04-28 14:06:28 +0000

    Implement getAllReservations using sql

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -102,7 +102,17 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+
+  return pool
+    .query(`SELECT (properties.*), reservations.start_date, reservations.end_date FROM properties JOIN reservations ON property_id = properties.id WHERE guest_id = $1 LIMIT $2`, [`${guest_id}`, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+  // return getAllProperties(null, limit);
 }
 exports.getAllReservations = getAllReservations;
 
